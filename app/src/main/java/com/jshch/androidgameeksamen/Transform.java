1ef447fbc453648becdda6f07ab5f66d5e7eddaf
Topper <tobi1995@live.dk>
2018-05-02 13:04:47 +0200

    Transform changed to fit needs

--- app/src/main/java/com/jshch/androidgameeksamen/Transform.java
+++ app/src/main/java/com/jshch/androidgameeksamen/Transform.java
@@ -1,8 +1,18 @@
 package com.jshch.androidgameeksamen;
 
 public class Transform {
+
     Vector2 Position;
     Vector2 GetPosition(){
         return Position;
     }
+    Vector2 scale;
+    public Transform(Vector2 position, Vector2 scale){
+        this.scale = scale;
+        this.Position = position;
+    }
+    void SetPosition(Vector2 position){
+        this.Position = position;
+    }
+
 }
