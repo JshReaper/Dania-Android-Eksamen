59b201cedc06143f044bf697bc9e372812110b04
Jacob SH <jshchansen@gmail.com>
2018-05-02 11:09:38 +0200

    GameWorld is singleton

--- app/src/main/java/com/jshch/androidgameeksamen/GameWorld.java
+++ app/src/main/java/com/jshch/androidgameeksamen/GameWorld.java
@@ -1,4 +1,15 @@
 package com.jshch.androidgameeksamen;
 
 public class GameWorld {
+   private static GameWorld instance;
+   protected GameWorld(){
+
+   }
+
+    public static GameWorld getInstance() {
+       if(instance == null){
+           instance = new GameWorld();
+       }
+       return instance;
+    }
 }
