0755d3f8bda4a43e52abf6ad3ea89ca551326c84
Jacob SH <jshchansen@gmail.com>
2018-05-02 13:51:09 +0200

    more draw

--- app/src/main/java/com/jshch/androidgameeksamen/GameView.java
+++ app/src/main/java/com/jshch/androidgameeksamen/GameView.java
@@ -1,7 +1,11 @@
 package com.jshch.androidgameeksamen;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.LinkedList;
@@ -16,9 +20,20 @@ public class GameView extends SurfaceView implements Runnable {
     CollisionChecker collisionChecker;
     // This is used to help calculate the fps
     private long timeThisFrame;
+    float deltaTime;
+    // This is new. We need a SurfaceHolder
+    // When we use Paint and Canvas in a thread
+    // We will see it in action in the draw method soon.
+    SurfaceHolder ourHolder;
+
+    // A Canvas and a Paint object
+    Canvas canvas;
+    Paint paint;
+
     public GameView(Context context) {
         super(context);
-
+        ourHolder = getHolder();
+        paint = new Paint();
     }
 
     @Override
@@ -47,11 +62,34 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void draw() {
+        if (ourHolder.getSurface().isValid()) {
+            // Lock the canvas ready to draw
+            // Make the drawing surface our canvas object
+            canvas = ourHolder.lockCanvas();
 
+            // Draw the background color
+            canvas.drawColor(Color.argb(255,  26, 128, 182));
+
+            // Choose the brush color for drawing
+            paint.setColor(Color.argb(255,  249, 129, 0));
+
+            // Make the text a bit bigger
+            paint.setTextSize(45);
+
+            // Display the current fps on the screen
+            canvas.drawText("FPS:" + fps, 20, 40, paint);
+
+            //draw all GO's
+            GameWorld.getInstance().Draw(canvas,paint);
+
+            // Draw everything to the screen
+            // and unlock the drawing surface
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
     }
 
     private void update() {
-
+        GameWorld.getInstance().Update(deltaTime);
     }
     // shutdown our thread.
     public void pause() {
