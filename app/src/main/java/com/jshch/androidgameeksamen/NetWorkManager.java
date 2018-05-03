0d5d10ad0eb6eee415840ca14a7b8ea6d480100c
Jacob SH <jshchansen@gmail.com>
2018-05-03 12:39:32 +0200

    networkmanager added

---
+++ app/src/main/java/com/jshch/androidgameeksamen/NetWorkManager.java
@@ -0,0 +1,38 @@
+package com.jshch.androidgameeksamen;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class NetWorkManager {
+   public static void HalloWorldExample(){
+        //setup database instance
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //generate reference to entry in database
+        DatabaseReference myRef = database.getReference("message");
+        //change value in the referenced entry
+        myRef.setValue("Hello, world: jacob");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("message", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("message", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+}
