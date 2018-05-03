8f84ae735970fff094e7e4a0b066fac802cd1599
Raunow <steff.bpoulsen@gmail.com>
2018-05-03 11:52:17 +0200

    Terrain

--- app/src/main/java/com/jshch/androidgameeksamen/Terrain.java
+++ app/src/main/java/com/jshch/androidgameeksamen/Terrain.java
@@ -1,9 +1,37 @@
 package com.jshch.androidgameeksamen;
 
-public class Terrain extends GameObject implements CollideAble, DrawAble, UpdateAble {
-    Terrain(Transform transform) {
-        super(transform);
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Terrain extends Component implements CollideAble, DrawAble, UpdateAble {
+
+
+    public Terrain(GameObject go) {
+        super(go);
+    }
+
+    @Override
+    public void Update(float deltaTime) {
 
     }
 
+    @Override
+    public void Draw(Canvas canvas, Paint paint){
+
+    }
+
+    @Override
+    public void OnCollisionEnter(Collider other) {
+
+    }
+
+    @Override
+    public void OnCollisionStay(Collider other) {
+
+    }
+
+    @Override
+    public void OnCollisionExit(Collider other) {
+
+    }
 }
