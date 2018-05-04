f50fc74fe113a015f47b2ffd8d3570e82f06e0cf
Raunow <steff.bpoulsen@gmail.com>
2018-05-04 11:24:15 +0200

    Terrain and TerrainBox

--- app/src/main/java/com/jshch/androidgameeksamen/GameObject.java
+++ app/src/main/java/com/jshch/androidgameeksamen/GameObject.java
@@ -9,7 +9,9 @@ public class GameObject {
 
     public String tag;
     LinkedList<Component> components;
-    private Transform transform;
+    Transform transform;
+
+
     Transform getTransform(){
         return transform;
     }
--- app/src/main/java/com/jshch/androidgameeksamen/Terrain.java
+++ app/src/main/java/com/jshch/androidgameeksamen/Terrain.java
@@ -1,37 +1,43 @@
 package com.jshch.androidgameeksamen;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-public class Terrain extends Component implements CollideAble, DrawAble, UpdateAble {
+import java.sql.Time;
+import java.util.ArrayList;
 
 
-    public Terrain(GameObject go) {
-        super(go);
-    }
+public class Terrain extends Component implements DrawAble, UpdateAble{
 
-    @Override
-    public void Update(float deltaTime) {
+    int NrBoxes = 20;
+    ArrayList<TerrainBox> boxes;
+    Bitmap BG_img, box_img;
 
+    public Terrain(GameObject go, Bitmap BG_img, Bitmap box_img) { super(go);
+        this.BG_img = BG_img;
+        this.box_img = box_img;
+
+        boxes = new ArrayList<>();
+        for (int i = 0; i < NrBoxes; i++){
+            boxes.add(new TerrainBox(new GameObject(new Transform(new Vector2(i * 10, 1000), 1f)), box_img));
+            //boxes.get(i);
+        }
     }
 
     @Override
     public void Draw(Canvas canvas, Paint paint){
+        canvas.drawBitmap(BG_img,go.getTransform().GetPosition().getX(),go.getTransform().GetPosition().getY(),paint );
 
+        for (int i = 0; i < NrBoxes; i++){
+            boxes.get(i).Draw(canvas, paint);
+        }
     }
 
     @Override
-    public void OnCollisionEnter(Collider other) {
-
-    }
-
-    @Override
-    public void OnCollisionStay(Collider other) {
-
-    }
-
-    @Override
-    public void OnCollisionExit(Collider other) {
-
+    public void Update(float deltaTime) {
+        for (int i = 0; i < NrBoxes; i++){
+            boxes.get(i).Update(deltaTime);
+        }
     }
 }
