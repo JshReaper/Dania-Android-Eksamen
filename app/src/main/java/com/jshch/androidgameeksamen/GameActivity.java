6aa53d07ca138188667bc7d9d1c4982ce00f68b3
Raunow <steff.bpoulsen@gmail.com>
2018-05-11 11:30:41 +0200

    Landscape

--- app/src/main/java/com/jshch/androidgameeksamen/GameActivity.java
+++ app/src/main/java/com/jshch/androidgameeksamen/GameActivity.java
@@ -1,11 +1,18 @@
 package com.jshch.androidgameeksamen;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.ConfigurationInfo;
+import android.content.res.Configuration;
+import android.media.VolumeShaper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
     GameView gameView;
+    View decorView;
+    int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,12 @@ public class GameActivity extends AppCompatActivity {
     }
     protected void onResume() {
         super.onResume();
-
+        
+        decorView = getWindow().getDecorView();
+        if(decorView != null){
+            decorView.setSystemUiVisibility(uiOptions);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
         // Tell the gameView resume method to execute
         gameView.resume();
     }
