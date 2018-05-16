dfa2c9d8bd23c958662c30a697470d3ef5b88674
Topper <tobi1995@live.dk>
2018-05-16 11:44:26 +0200

    Collision is optimized but stay was never called in the first place for some reason...

--- app/src/main/java/com/jshch/androidgameeksamen/Collider.java
+++ app/src/main/java/com/jshch/androidgameeksamen/Collider.java
@@ -27,9 +27,9 @@ public class Collider extends Component {
         }
         Bitmap otherBitMap =otherRenderer.GetBitmap();
         float myCenterX = go.getTransform().GetPosition().getX() + bitmap.getWidth() /2;
-        float OtherCenterX = go.getTransform().GetPosition().getX() + otherBitMap.getWidth()/2;
+        float OtherCenterX = otherRenderer.GetGameObject().getTransform().GetPosition().getX() + otherBitMap.getWidth()/2;
         float myCenterY = go.getTransform().GetPosition().getY() + bitmap.getHeight() /2;
-        float OtherCenterY = go.getTransform().GetPosition().getY() + otherBitMap.getHeight()/2;
+        float OtherCenterY = otherRenderer.GetGameObject().getTransform().GetPosition().getY() + otherBitMap.getHeight()/2;
         if (Math.abs(myCenterX-OtherCenterX)<=(bitmap.getWidth()+otherBitMap.getWidth())/2f
                 && Math.abs(myCenterY-OtherCenterY)<=(bitmap.getHeight()+otherBitMap.getHeight())/2f)
             return true;
