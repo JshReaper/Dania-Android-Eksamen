924dc70f6308f4890adfe092e493bf0e2e3183e9
Topper <tobi1995@live.dk>
2018-05-30 10:07:59 +0200

    changed it so toutch under the 150 height is ignored in rotation

--- app/src/main/java/com/jshch/androidgameeksamen/TankController.java
+++ app/src/main/java/com/jshch/androidgameeksamen/TankController.java
@@ -10,7 +10,6 @@ public class TankController extends Component implements UpdateAble, ControlAble
     boolean myTurn = true;
     Vector2 firstPos;
     Tank myTank;
-    boolean toutch;
     boolean fire = false;
 
     public TankController(GameObject go){
@@ -24,7 +23,6 @@ public class TankController extends Component implements UpdateAble, ControlAble
             if(firstPos != null){
                     Vector2 temp = firstPos.Subtract(myTank.turretOffset);
                     angle = temp.AngleToVector2D(new Vector2(10, 0));
-                Log.d("MSG","TEMP vector is:" + angle);
                     if (angle < 0) {
                         angle = 0;
                     } else if (angle > 180) {
@@ -44,10 +42,9 @@ public class TankController extends Component implements UpdateAble, ControlAble
     @Override
     public void Controller(float x, float y, boolean isToutched){
 
-        if(isToutched) {
+        if(isToutched && y > 150) {
 
                 firstPos = new Vector2(x, y);
-            Log.w("firstpos: ", ""+ firstPos.getX()+","+firstPos.getY());
         }
     }
 
